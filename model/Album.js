ef8db0526b68687904bde12a87a09dd40d3ec681
Magali <fort_magali@hotmail.fr>
2021-03-02 12:02:56 +0100

    add album model

--- model/Album.js
+++ model/Album.js
@@ -1,9 +1,17 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const ArtistModel = require("./Artist");
+const LabelModel = require("./Label");
 
 // todo : fill the Scheam below !
 
-const albumSchema = new Schema();
+const albumSchema = new Schema( {
+    title: String,
+    releaseDate: Date,
+    label: { type: Schema.Types.ObjectId, ref: "labels" },
+    artist: { type: Schema.Types.ObjectId, ref: "artists" },
+    cover: { type: String, default: "https://res.cloudinary.com/gdaconcept/image/upload/v1614550771/workshop-artistify/no-image-logo_dcufai.png"}
+});
 
 const AlbumModel = mongoose.model("album", albumSchema);
 
